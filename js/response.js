7399ae3e4ceda1c9af39cd8efcd65e01df68797e
CyberQue <73695427+CyberQue@users.noreply.github.com>
2020-11-06 17:38:07 +0530

    Add files via upload

---
+++ js/response.js
@@ -0,0 +1,11 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navImage = document.querySelector('.navImage')
+navList = document.querySelector('.nav-list') 
+
+burger.addEventListener('click', ()=>{
+    navList.classList.toggle('v-class-resp');
+    navImage.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+
